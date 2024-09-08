8849b88018ff8c5518d919d152b8359f6e01fbe8
Frederik Dahl Hansen <hansenf@shernet.sheridancollege.ca>
2024-09-07 21:48:49 -0400

    Initial Commit

---
+++ Application Tracker/Application_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Application_TrackerApp.swift
+//  Application Tracker
+//
+//  Created by Frederik Dahl Hansen on 07/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Application_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Application Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Application Tracker
+//
+//  Created by Frederik Dahl Hansen on 07/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
