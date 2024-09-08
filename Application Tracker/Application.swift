a3414054b655bc343751e99124713ed9a882bcfe
Frederik Dahl Hansen <hansenf@shernet.sheridancollege.ca>
2024-09-07 22:00:25 -0400

    Set up initial class for the tracker as well as the enum used to indicate progress in application

---
+++ Application Tracker/Application.swift
@@ -0,0 +1,8 @@
+//
+//  Application.swift
+//  Application Tracker
+//
+//  Created by Frederik Dahl Hansen on 07/09/2024.
+//
+
+import Foundation
---
+++ Application Tracker/Status.swift
@@ -0,0 +1,8 @@
+//
+//  Status.swift
+//  Application Tracker
+//
+//  Created by Frederik Dahl Hansen on 07/09/2024.
+//
+
+import Foundation
